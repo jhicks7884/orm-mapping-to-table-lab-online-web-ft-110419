0589771d411c7189152b667c4c6b6d66525c1b34
Jeshua Hicks <jhicks7884@outlook.com>
2019-12-27 17:28:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,12 +10,12 @@ end
 def self.create_table
   sql = <<-sql
   CREATE TABLE IF NOT EXISTS songs (
-       id INTEGER PRIMARY KEY, 
-       name TEXT, 
+       id INTEGER PRIMARY KEY,
+       name TEXT,
        album TEXT
        )
        SQL
-   DB[:conn].execute(sql) 
+   DB[:conn].execute(sql)
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
