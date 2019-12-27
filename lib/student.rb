1fdecef9386dfeeee5821603d43177747c4035bb
Jeshua Hicks <jhicks7884@outlook.com>
2019-12-27 17:36:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,4 +21,12 @@ def self.create_table
   #  with DB[:conn]
 end
 
+ def save
+   sql = <<-SQL
+      INSERT INTO students (name, grade) 
+      VALUES (?, ?)
+    SQL
+ 
+    DB[:conn].execute(sql, self.name, self.grade)
+
 end
