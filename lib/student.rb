43af22d134e7954a765657f3ce19a067e1926f08
Jeshua Hicks <jhicks7884@outlook.com>
2019-12-27 17:28:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,6 +6,16 @@ def initialize(name, grade, id=nil)
   @grade = grade
   @id = id
 end
+
+def self.create_table
+  sql = <<-sql
+  CREATE TABLE IF NOT EXISTS songs (
+       id INTEGER PRIMARY KEY, 
+       name TEXT, 
+       album TEXT
+       )
+       SQL
+   DB[:conn].execute(sql) 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
